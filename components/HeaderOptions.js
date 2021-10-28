fd791e417cbc016c93f89c66a17d01c8d8f0e27e
Ziyaad <rhyman.ziyaad@gmail.com>
2021-10-28 10:26:56 +0400

    [+][ADD] Building header on search page

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -3,7 +3,7 @@ import HeaderOption from '../components/HeaderOption';
 
 function HeaderOptions() {
     return (
-        <div className="flex w-full text-gray-700 justify-evenly">
+        <div className="flex w-full text-gray-700 justify-evenly text-sm lg:text-base lg-justify-start lg:space-x-36 lg:pl-52 border-b">
             {/* Left */}
             <div className="flex space-x-6">
                 <HeaderOption Icon={SearchIcon} title="All" selected />
