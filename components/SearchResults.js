42d1f22929bae3501c1a89930899214f169697b5
Ziyaad <rhyman.ziyaad@gmail.com>
2022-04-10 13:59:59 +0400

    [+][ADD] Added search results

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,7 +1,17 @@
 function SearchResults({ results }) {
     return (
-        <div>
+        <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
+            <p className="text-gray-600 text-md mb-5 mt-3">About {results.searchInformation?.formattedTotalResults} results ({results.searchInformation?.formattedSearchTime} seconds)</p>
 
+            {results.items?.map((result) => (
+                <div key={result.link} className="max-w-xl mb-8">
+                    <div className="group">
+                        <a href={result.link} className="text-sml">{result.formattedUrl}</a>
+                        <a href={result.link}><h2 className="truncate text-xl text-blue-600 font-medium group-hover:underline">{result.title}</h2></a>
+                    </div>
+                    <p>{result.snippet}</p>
+                </div>
+            ))}
         </div>
     );
 }
